fcf925247d5ecb999e8fafcf6cf46b8e8dce8d1c
JAG <jagrasso42@gmail.com>
2022-10-22 13:21:53 -0400

    Added media queries and made menu overlay responsive

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,6 +5,8 @@ const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
+// ASSIGNMENT CODE
+
 // Set initial state of menu
 let showMenu = false;
 
